8cb7e768eed8124f0c74006ad137e849da56f560
Prabhat Padhiary <prabhatpadhiary@gmail.com>
2022-07-16 01:57:28 +0530

    Add files via upload

---
+++ CSS/Try.py
@@ -0,0 +1,10 @@
+# a=int(input("enter your first number"))
+# b=int(input("enter your second number"))
+# c=input("what is the operation")
+# if c == "+" or c == "addition":
+#     print(a+b)
+# elif  c == "-":
+#     print(a-b)
+# elif c == "*":
+#     print(a*b)
+     
